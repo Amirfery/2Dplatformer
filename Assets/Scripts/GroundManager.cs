c6a79d4520f3ad78333f70bf5bb43ce2099fcadc
Behrad fathi <fathibehrad@gmail.com>
2020-08-29 23:55:55 +0430

    platform added

--- Assets/Scripts/GroundManager.cs
+++ Assets/Scripts/GroundManager.cs
@@ -1,13 +1,20 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class GroundManager : MonoBehaviour
 {
-    public GameObject Platform;
+    public GameObject Platform1;
+    public GameObject Platform2;
+    public GameObject Platform3;
+    List<GameObject> Platforms = new List<GameObject>();
     public float countdown4 = 4f;
 
     void Start()
     {
-
+        Platforms.Add(Platform1);
+        Platforms.Add(Platform2);
+        Platforms.Add(Platform3);
     }
 
     void Update()
@@ -15,14 +22,14 @@ public class GroundManager : MonoBehaviour
         countdown4 -= Time.deltaTime;
         if (countdown4 <= 0)
         {
-            Spawn();
+            Spawn(Random.Range(0, 2));
             countdown4 = 4f;
         }
     }
-    void Spawn()
+    void Spawn(int i)
     {
-        Vector3 pos = new Vector3(10, Random.Range(-1 , -4), 0);
-        GameObject goodsPrefab = Platform;
+        Vector3 pos = new Vector3(16, Random.Range(-1.5f , 0.6f), 0);
+        GameObject goodsPrefab = Platforms[i];
         Instantiate(goodsPrefab, pos, goodsPrefab.transform.rotation);
     }
 }
